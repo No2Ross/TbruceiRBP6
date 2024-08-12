87185f683ce239900f13d1cec03c81708a7f50fe
No2Ross <78097380+No2Ross@users.noreply.github.com>
2024-08-12 12:01:32 +0100

    Add files via upload
    Added the script for mapping the RBP6 OE 10X scRNA-seq data against the reference using Cellranger 10X Count

---
+++ Scripts/RBP6_OE_analysis/RBP6_cellranger_count_mVSG.sh
@@ -0,0 +1,3 @@
+source /export/projects/bioinfo2/to16r/bin/cellranger-7.0.0/sourceme.bash
+cellranger count --localcores=20 --localmem=40 --id=RBP6_mVSG --fastqs=/export/projects/III-data/otto/2254704l/RBP6_dataset/2023_run/merge_fastq --transcriptome=/export/projects/III-data/otto/2254704l/10x_references/Tb927_2500utr_mVSG  --sample=RBP-6
+
